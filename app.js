21cc2c0f396f36f1bd295ee4f86cbd3b5b9a9236
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-03-22 12:27:26 -0400

    Remved db connection code from app.js file

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongodb = require("mongodb").MongoClient;
 
 const productRoutes = require("./routes/products");
 const authRoutes = require("./routes/auth");
@@ -25,13 +24,5 @@ app.use((req, res, next) => {
 
 app.use("/products", productRoutes);
 app.use("/", authRoutes);
-
-mongodb
-  .connect("mongodb://mongodb:27017/shop")
-  .then((client) => {
-    console.log("Connected!");
-    client.close();
-  })
-  .catch((err) => console.log(err));
 
 app.listen(3100);
